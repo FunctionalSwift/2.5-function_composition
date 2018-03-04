c6bbca67751c6dd2f815d4c0773679ea191ba2b1
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 13:47:02 +0100

    Starting with imperative programming... we don't like it :(

--- Function Composition.playground/Contents.swift
+++ Function Composition.playground/Contents.swift
@@ -1,1 +1,36 @@
 //: Playground - Function Composition
+
+import Foundation
+
+let prices = "[\"10\", \"5\", \"null\", \"20\", \"0\"]"
+
+func formatPrices(json: String) -> [String] {
+    let data = json.data(using: .utf8)!
+    let jsonArray = try! JSONSerialization.jsonObject(with: data) as! [String]
+    
+    var labels = [String]()
+    
+    for element in jsonArray {
+        let price = Int(element)
+        
+        if let price = price {
+            var label = ""
+            
+            if price == 0 {
+                label = "Free"
+            }
+            else {
+                let numberFormatter = NumberFormatter()
+                numberFormatter.numberStyle = .currency
+                numberFormatter.locale = Locale(identifier: "ES_es")
+                
+                label = numberFormatter.string(from: price as NSNumber)!
+            }
+            
+            labels.append(label)
+        }
+    }
+    return labels
+}
+
+formatPrices(json: prices)
